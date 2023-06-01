e976b3b47a4a5d8cdbdbd42d50bce4e93cf3d3c7
Adrian463588 <adriansyah2020@mail.ugm.ac.id>
2023-06-01 21:28:55 +0700

    add admin feature

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import './App.css';
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './components/HomePage';
@@ -7,15 +6,14 @@ import ProductPage from './components/ProductPage';
 import SignupPage from './components/SignupPage';
 import ProfilePage from './components/ProfilePage';
 import BookList from './components/BookList';
-
-
-
-
+import AdminDashboard from './components/AdminDashboard';
+import UpdateBook from './components/updateBook';
+import DeleteBook from './components/DeleteBook';
+import AddBook from './components/AddBook';
+import 'tailwindcss/tailwind.css';
+import './App.css';
 
 const App: React.FC = () => {
-
-  
-
   return (
     <Router>
       <Routes>
@@ -25,8 +23,16 @@ const App: React.FC = () => {
         <Route path="/signup" element={<SignupPage />} />
         <Route path="/profile" element={<ProfilePage />} />
         <Route path="/booklist" element={<BookList />} />
-        
- 
+        <Route path="/admin" element={<AdminDashboard />} />
+        <Route path="/updatebook" element={<UpdateBook title={''} details={''} cover={''} onUpdate={function (title: string, details: string, cover: string): void {
+          throw new Error('Function not implemented.');
+        } } />} />
+        <Route path="/deletebook" element={<DeleteBook cover={''} onDelete={function (): void {
+          throw new Error('Function not implemented.');
+        } } />} />
+        <Route path="/addbook" element={<AddBook onAdd={function (title: string, author: string, details: string, cover: string): void {
+          throw new Error('Function not implemented.');
+        } } />} />
       </Routes>
     </Router>
   );
